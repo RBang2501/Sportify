c8aae6a584cf8258ccea50081b5ecbbd7a5e173c
Dragonglass101 <79797011+Dragonglass101@users.noreply.github.com>
2023-07-19 17:12:48 +0530

    auth with jwt working

---
+++ backend/src/main/java/com/sportify/backendauth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sportify.backendauth.repository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sportify.backendauth.models.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
+}
