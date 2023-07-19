2eb17d6c4307093aafec05671eb904d2e5a44497
Dragonglass101 <79797011+Dragonglass101@users.noreply.github.com>
2023-07-19 16:55:47 +0530

    adding getters and setters

--- backend/src/main/java/com/sportify/backendauth/response/JwtResponse.java
+++ backend/src/main/java/com/sportify/backendauth/response/JwtResponse.java
@@ -14,4 +14,45 @@ public class JwtResponse {
 		this.email = email;
 
 	}
+
+	public String getAccessToken() {
+		return token;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.token = accessToken;
+	}
+
+	public String getTokenType() {
+		return type;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.type = tokenType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 }
